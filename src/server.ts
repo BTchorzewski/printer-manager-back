59b81e8950c7660110e7c7ab20dee8332a5c25ca
BTchorzewski <tchorzewskibartosz@gmail.com>
2022-06-20 21:45:08 +0200

    created connection to DB.

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
 import app from './app';
 import {config} from './config/config';
+import {AppDataSource} from './utils/db'
 
+
+AppDataSource.initialize().then(res => console.log('Database connected.'))
 app.listen(config.appPort ?? 3001, () => console.log('app listening on port 3001'))
