fc3e0eee4199107fe7040d55804ec6a1be1f682e
BTchorzewski <tchorzewskibartosz@gmail.com>
2022-06-23 18:54:48 +0200

    created interfaces for printer and supply.

--- types/printer/index.ts
+++ types/printer/index.ts
@@ -1,7 +1,20 @@
+import { Supply } from '../supply';
+
 export type PrinterTest = string;
 
 export enum PrinterModel {
   Xerox_AltaLink_C8035 = 'Xerox AltaLink C8035',
   Xerox_VersaLink_C605 = 'Xerox VersaLink C605',
   Xerox_VersaLink_C400 = 'Xerox VersaLink C400',
+}
+
+export interface Printer {
+  id: string;
+  name: string;
+  ip: string;
+  isMultifunctional: boolean;
+  area: string;
+  location: string;
+  model: PrinterModel;
+  supplies: Supply[];
 }
--- types/supply/index.ts
+++ types/supply/index.ts
@@ -1,1 +1,12 @@
-export type SupplyTest = string;
+import { PrinterModel } from '../printer';
+
+export interface Supply {
+  id: string;
+  name: string;
+  code: string;
+  isAvailable: boolean;
+  model: PrinterModel;
+  storedAt: Date;
+  installedAt: Date;
+  printerId?: string;
+}
