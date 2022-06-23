317ab40c01bb11ab8a11b7f94ad980924b537c1f
BTchorzewski <tchorzewskibartosz@gmail.com>
2022-06-23 18:50:56 +0200

    created entity for printer.

---
+++ src/entities/printer.entity.ts
@@ -0,0 +1,33 @@
+import { PrinterModel } from '../../types';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { SupplyEntity } from './supply.entity';
+
+@Entity()
+export class PrinterEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ length: 5 })
+  name: string;
+
+  @Column({
+    type: 'enum',
+    enum: PrinterModel,
+  })
+  model: PrinterModel;
+
+  @Column({ length: 16 })
+  ip: string;
+
+  @Column()
+  isMultifunctional: boolean;
+
+  @Column({ length: 150 })
+  area: string;
+
+  @Column({ length: 500 })
+  location: string;
+
+  @OneToMany(() => SupplyEntity, supply => supply.id)
+  supplies: SupplyEntity[]
+}
