a74d36d7a7f15ad8080e765d04e03b932beb7ba6
BTchorzewski <tchorzewskibartosz@gmail.com>
2022-06-23 16:33:08 +0200

    created enum with models of printers

--- types/printer/index.ts
+++ types/printer/index.ts
@@ -1,1 +1,7 @@
 export type PrinterTest = string;
+
+export enum PrinterModel {
+  Xerox_AltaLink_C8035 = 'Xerox AltaLink C8035',
+  Xerox_VersaLink_C605 = 'Xerox VersaLink C605',
+  Xerox_VersaLink_C400 = 'Xerox VersaLink C400',
+}
