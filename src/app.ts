fd8707615787f8217b0b5f85adaea7354e3ed372
BTchorzewski <tchorzewskibartosz@gmail.com>
2022-06-27 17:20:18 +0200

    deleted old db file.

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express, { json } from 'express';
 import { config } from './config/config';
 import { errorHandler } from './utils/error-handler';
 import printerRouter from './routes/printer.router';
+import supplyRouter from './routes/supply.router';
 
 const app = express();
 
@@ -14,7 +15,8 @@ app.use(cors({
   origin: config.corsOrigin ?? 'http://localhost:3001/'
 }))
 
-app.use('/api', printerRouter)
+app.use('/api', printerRouter);
+app.use('/api', supplyRouter);
 
 app.use(errorHandler)
 export default app;
