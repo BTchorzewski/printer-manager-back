b8c60b672c17bc0b0ea0ff274560a5d991d35d1c
BTchorzewski <tchorzewskibartosz@gmail.com>
2022-07-03 21:46:02 +0200

    changed cors configuration.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import cors from 'cors';
-import express, { json } from 'express';
-import { config } from './config/config';
-import { errorHandler } from './utils/error-handler';
+import express, {json} from 'express';
+import {config} from './config/config';
+import {errorHandler} from './utils/error-handler';
 import printerRouter from './routes/printer.router';
 import supplyRouter from './routes/supply.router';
 import storeRouter from './routes/store.router';
@@ -13,7 +13,9 @@ app.use(json())
 
 // cors
 app.use(cors({
-  origin: config.corsOrigin ?? 'http://localhost:3001/'
+  origin: config.corsOrigin ?? '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  preflightContinue: false,
 }))
 
 app.use('/api', printerRouter);
