fc90bd196476a6d1c3d77a9d085dc2a323770737
BTchorzewski <tchorzewskibartosz@gmail.com>
2022-07-04 23:09:48 +0200

    added status code to error response.

--- src/utils/error-handler.ts
+++ src/utils/error-handler.ts
@@ -1,11 +1,11 @@
-import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
+import {ErrorRequestHandler, NextFunction, Request, Response} from 'express';
 
 export class ValidationError extends Error {
 }
 
 export const errorHandler = (error: ErrorRequestHandler, req: Request, res: Response, next: NextFunction) => {
+  res.status(error instanceof ValidationError ? 400 : 500)
   res.json({
     msg: error instanceof ValidationError ? error.message : 'Come back later',
-    errorCode: error instanceof ValidationError ? 400 : 500,
   })
 }
