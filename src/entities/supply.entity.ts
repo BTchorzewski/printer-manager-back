e1a18cb5e50e63a360592ab52b595fd2fca87782
BTchorzewski <tchorzewskibartosz@gmail.com>
2022-07-13 15:00:43 +0200

    added enum to entity

--- src/entities/supply.entity.ts
+++ src/entities/supply.entity.ts
@@ -1,5 +1,5 @@
 import { PrinterModel } from '../../types';
-import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntity, Column, Entity, JoinColumn, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { StoreEntity } from './store.entity';
 
 @Entity()
@@ -10,12 +10,16 @@ export class SupplyEntity extends BaseEntity {
   @Column()
   name: string;
 
-  @Column()
+  @Column({
+    type: 'enum',
+    enum: PrinterModel,
+  })
   model: PrinterModel;
 
   @Column()
   code: string;
   //@todo rename stores to stored.
   @OneToMany(() => StoreEntity, store => store.supply)
+  @JoinColumn()
   stores: StoreEntity[];
 }
